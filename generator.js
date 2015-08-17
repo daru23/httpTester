184c1b0cf171656e489902ee03627d1ee872fc04
dans <dans@localhost.localdomain>
2015-08-17 21:07:42 +0200

    test

--- generator.js
+++ generator.js
@@ -10,7 +10,7 @@ exports.generateRamdomData = function (){
 // function getRandomArbitrary(min, max) {
 //   return Math.random() * (max - min) + min;
 //   }
-//   
+   
 {POSUniqsdfdsfsdfsdfueID : "aloha", ClientID: 1000001, Hash: "abcd01234abcd5678" }
 var example = {"TargetDate": "2015-08-17","TargetTime": "1234567890","POSID": 1,"POSName": "futurepos","Reports": {"Turnover": 321,"Hourly": [{"time":"Mon Aug 17 2015 12:17:07 GMT+0200 (CEST)", "turnover": 100, "tax":20},{"time":"Mon Aug 17 2015 13:00:00 GMT+0200 (CEST)", "turnover": 70, "tax":20}],"Departments":[{"departmentName":"Bar","departmentID":101, "turnover":90}]}};
 
